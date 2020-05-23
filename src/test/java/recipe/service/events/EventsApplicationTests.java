7c53dcd2f9793f53cd9ba521990486ca7edd39a4
Martin Fabián <martin.fabian@centrum.cz>
2020-05-24 00:25:46 +0200

    Initial commit

---
+++ src/test/java/recipe/service/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package recipe.service.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
